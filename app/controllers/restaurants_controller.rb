606d6c901f56a17b6efda0108a962a7f404991cc
pierrefrancois thomas <pf-thomas@hotmail.fr>
2023-11-15 14:34:53 +0100

    first update

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,37 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new # Needed to instantiate the form_with
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save # Will raise ActiveModel::ForbiddenAttributesError
+
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+    # No need for app/views/restaurants/update.html.erb
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :rating, :category, :phone_number)
+  end
+end
